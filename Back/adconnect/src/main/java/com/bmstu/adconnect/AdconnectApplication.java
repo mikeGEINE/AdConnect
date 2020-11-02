ce308587d36c4fae26ad694e4897194e65f0cb1c
Mikhail Geine <mike.geine@gmail.com>
2020-11-02 14:25:54 +0300

    Basic spring boot application with Maven

---
+++ Back/adconnect/src/main/java/com/bmstu/adconnect/AdconnectApplication.java
@@ -0,0 +1,13 @@
+package com.bmstu.adconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdconnectApplication.class, args);
+	}
+
+}
